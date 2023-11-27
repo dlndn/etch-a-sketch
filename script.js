92fd5e5dc2c190e03700b070f77c286270162e41
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-27 15:23:25 -0800

    added sketchpad generator button

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let container = document.querySelector('#container');
-
-for (let i=0; i<16; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-
-    for (let j=0; j<16; j++) {
-        let column = document.createElement('div');
-        column.classList.add('column');
-        column.addEventListener('mouseover', () =>  column.style.backgroundColor =  'black');
-
-        row.appendChild(column);
+function createGrid() {
+    let container = document.querySelector('#container');
+    
+    for (let i=0; i<16; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+    
+        for (let j=0; j<16; j++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            column.addEventListener('mouseover', () =>  column.style.backgroundColor =  'black');
+    
+            row.appendChild(column);
+        }
+    
+        container.appendChild(row);
     }
-
-    container.appendChild(row);
 }
