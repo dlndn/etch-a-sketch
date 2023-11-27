07a71fe610e7613784381bb6c1e80578ea6590d0
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-27 15:13:18 -0800

    created 16x16 grid pad

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etch-a-sketch
+This is an assignment for The Odin Project. Simulates a sketchpad and/or etch-a-sketch on a webpage.
---
+++ script.js
@@ -0,0 +1,16 @@
+let container = document.querySelector('#container');
+
+for (let i=0; i<16; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j=0; j<16; j++) {
+        let column = document.createElement('div');
+        column.classList.add('column');
+        column.addEventListener('mouseover', () =>  column.style.backgroundColor =  'black');
+
+        row.appendChild(column);
+    }
+
+    container.appendChild(row);
+}
