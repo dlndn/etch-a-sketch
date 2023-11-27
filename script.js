16c1c1b588afe50f61202515b1bc184c5c7f8f61
dlndn <65935030+dlndn@users.noreply.github.com>
2023-11-27 15:47:24 -0800

    added button for custom sketchpad

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function createGrid() {
     let container = document.querySelector('#container');
-    
-    for (let i=0; i<16; i++) {
+    let pixels = parseInt(prompt("Please enter a number of the pixel length of the square sketchpad. (Min: 1, Max: 100)"));
+
+    if (isNaN(pixels) || pixels === null 
+        || 1 > pixels || pixels > 100) {
+        alert("Invalid input entered. Please enter a number between 1 and 100.");
+        return;
+    }
+
+    for (let i=0; i<pixels; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
     
-        for (let j=0; j<16; j++) {
+        for (let j=0; j<pixels; j++) {
             let column = document.createElement('div');
             column.classList.add('column');
             column.addEventListener('mouseover', () =>  column.style.backgroundColor =  'black');
